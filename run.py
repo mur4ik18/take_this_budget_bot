41110c41d787bcd570c585167708f594753e82f5
mur4ik18 <o.o.kotov1999@gmail.com>
2023-06-01 19:38:47 +0200

    added new request and verification
    added verification if user used command /start for creating his database
    added new request - print last 10 payments

--- run.py
+++ run.py
@@ -36,21 +36,40 @@ def help(message):
     bot.reply_to(message, "help")
 
 
+@bot.message_handler(commands=['last_10'])
+def last_10(message):
+    db = Database()
+    id = get_chat_id(message)
+    if not db.check_table(f"payments{id}"):
+        bot.reply_to(message, "Use /start for create your own db")
+    last = db.return_last_payments(id, 10)
+    reponse = ""
+    print(last)
+    for i in last:
+        reponse += f"{i[4].split(' ')[0]} - {i[3]} - {i[1]} - {i[2]}\n"
+    bot.reply_to(message, reponse)
+
+
 @bot.message_handler(commands=['payed'])
 def payed(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
+    db = Database()
+    id = get_chat_id(message)
+    if not db.check_table(f"payments{id}"):
+        bot.reply_to(message, "Use /start for create your own db")
+    else:
+        markup = types.InlineKeyboardMarkup(row_width=2)
 
-    item0 = types.InlineKeyboardButton("Еда", callback_data='food')
-    item = types.InlineKeyboardButton("Химия", callback_data='chimy')
-    item1 = types.InlineKeyboardButton("Хотелки", callback_data='nyam')
-    item2 = types.InlineKeyboardButton("Развлечения", callback_data='fun')
-    item3 = types.InlineKeyboardButton("Инвестиции", callback_data='investing')
-    item4 = types.InlineKeyboardButton("Медицина", callback_data='health')
-    item5 = types.InlineKeyboardButton("Повседневное",
-                                       callback_data='everyday')
-    markup.add(item0, item, item1, item2, item3, item4, item5)
+        item0 = types.InlineKeyboardButton("Еда", callback_data='food')
+        item = types.InlineKeyboardButton("Химия", callback_data='chimy')
+        item1 = types.InlineKeyboardButton("Хотелки", callback_data='nyam')
+        item2 = types.InlineKeyboardButton("Развлечения", callback_data='fun')
+        item3 = types.InlineKeyboardButton("Инвестиции", callback_data='investing')
+        item4 = types.InlineKeyboardButton("Медицина", callback_data='health')
+        item5 = types.InlineKeyboardButton("Повседневное",
+                                           callback_data='everyday')
+        markup.add(item0, item, item1, item2, item3, item4, item5)
 
-    bot.reply_to(message, "За что ты заплатил(а)", reply_markup=markup)
+        bot.reply_to(message, "За что ты заплатил(а)", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
